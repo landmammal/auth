730bea9ee1fc33e835fb4680b68097bfb1cedaf7
landmammal <evolpirate@gmail.com>
2020-02-24 11:26:07 -0500

    barrow my credentials

--- app.js
+++ app.js
@@ -7,7 +7,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
     // your keys here
- 
+    apiKey: "AIzaSyCyK0o3xcA2ufZfSq90ytJnQ_udRMC_tfM",
+    authDomain: "user-authentication-9fc60.firebaseapp.com",
+    databaseURL: "https://user-authentication-9fc60.firebaseio.com",
+    projectId: "user-authentication-9fc60",
+    storageBucket: "user-authentication-9fc60.appspot.com",
+    messagingSenderId: "903367526321",
+    appId: "1:903367526321:web:a518c7466774f9a4941f90"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -26,4 +32,5 @@ formSubmit.on('click', function(){
     var password = passText.val();
     console.log('LOL');
     auth.createUserWithEmailAndPassword(email, password);
+    
 });
