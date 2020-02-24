d11fafef2fc31ecdeb2ee1c0e3d174e4367ad245
landmammal <evolpirate@gmail.com>
2020-02-24 11:23:48 -0500

    user registration

--- app.js
+++ app.js
@@ -7,9 +7,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
     // your keys here
+ 
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// service reference
+var auth = firebase.auth()
 
 
 var emailText = $('#exampleInputEmail1');
@@ -18,6 +22,8 @@ var formSubmit = $('#submitBtn');
 
 formSubmit.on('click', function(){
     event.preventDefault();
+    var email = emailText.val();
+    var password = passText.val();
     console.log('LOL');
-    
+    auth.createUserWithEmailAndPassword(email, password);
 });
